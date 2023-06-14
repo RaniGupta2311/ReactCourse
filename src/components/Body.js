5cbab866b122d3ca8ca9f6d324088e8005f17d3e
Rani Gupta <rani.gupta259@gmail.com>
2023-06-15 01:47:44 +0530

    C6 explore the world

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,18 +1,62 @@
 import { restaurantList } from "../constants";
 import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
+import { useState,useEffect } from "react";
+import Shimmer from "./Shimmer"
 
-function filterData(searchText,restaurants){
-    const filterdData=restaurants.filter((res)=>res.data.name.includes(searchText))
-    return filterdData;
+function filterData(sText,rest){
+  const filteredData=rest.filter((res)=>res?.data?.name.toLowerCase().includes(sText.toLowerCase()))
+  return filteredData;
 }
+
 const Body = () => {
-  //   let searchTxt = "KFC";
-  // searchText is a local state variable
   const [searchText, setSearchText] = useState("");
-  const [searchClicked, setSearchClicked] = useState("false");
-  const [restaurants, setRestaurants] = useState(restaurantList);
-  return (
+  // showing old data before api call
+  // const [restaurants, setRestaurants] = useState(restaurantList);
+  // this wont show any data
+  const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+  const [allRestaurants,setAllRestaurants]=useState([]);
+
+  // console.log("render-body")
+  // console.log(restaurants)
+
+  // this will be called only once (just initial render)
+  // useEffect(()=>{
+  //   console.log("call this when dependency is chaged")
+  // },[])
+
+  // this will be called when we enter anything in search input
+  // useEffect(()=>{
+  //   console.log("call this when dependency is chaged")
+  // },[searchText])
+
+  // empty dependency array=> once after render
+  // dependency array [searchText]=> once after initial render + everytime after render(when searchText changes)
+  // useEffect(()=>{
+  //   console.log("useEffect Hook")
+  // },[restaurants])
+
+  // making api call
+  useEffect(()=>{
+    // api call
+    getRestaurants()
+  },[])
+
+  async function getRestaurants(){
+    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=24.748163724143655&lng=84.38034899532796&page_type=DESKTOP_WEB_LISTING");
+    const json=await data.json();
+    console.log(json);
+    // use optional chaining
+    // console.log(json?.data?.cards[0]?.data?.data?.cards);
+    setAllRestaurants(json?.data?.cards[0]?.data?.data?.cards);
+    setFilteredRestaurants(json?.data?.cards[0]?.data?.data?.cards);
+  }
+
+  console.log("render");
+  if (!allRestaurants) return null
+    // conditional rendering
+  // we are showing shimmer UI if data is not loaded from api call
+  return (allRestaurants.length===0)?<Shimmer/> :
+  (
     <>
       <div className="search-container">
         <input
@@ -28,27 +72,20 @@ const Body = () => {
         <button
          className="search-btn"
          onClick={()=>{
-            //  filter data
-            const data=filterData(searchText,restaurants);
-            // update the state restaurannts
-            setRestaurants(data);
-         }}>
+          //  filter data
+          const data=filterData(searchText,allRestaurants)
+          setFilteredRestaurants(data)
+         }}
+       >
         Search
         </button>
-        {/* <h4>{searchText}</h4>  */}
-
-        {/* <button onClick={()=>{
-            if(searchClicked==="true")
-            setSearchClicked("false")
-            else
-            setSearchClicked("true")
-        }}>Toggle</button>
-        <h2>{searchClicked}</h2> */}
       </div>
 
       <div className="restaurant-list">
-        {restaurants.map((res) => {
-          return <RestaurantCard key={res.data.id} {...res.data} />;
+        {filteredRestaurants.map((res) => {
+          return (filteredRestaurants.length===0)? <h1>No match found</h1> : (
+            <RestaurantCard key={res.data.id} {...res.data} />
+          );
         })}
       </div>
     </>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,7 @@
+import {useState} from "react"
+const loggedInUser=()=>{
+  return true;
+}
 const Title=()=>{
     return (
       <a href="/"><img src="https://obs.line-scdn.net/0m0339dd5b72513e8a8d6127b836e5ecf1e128f1da5cb7?0" alt="logo" className="logo"/></a>
@@ -5,9 +9,16 @@ const Title=()=>{
   }
 
 const Header=()=>{
+  const [isLoggedIn,setIsLoggedIn]=useState(false);
+  // let title="Food Villa"
+  const [title,setTitle]=useState("FoodVilla");
+  // console.log("render");
     return (
       <div className="header">
         <Title/>
+        <h1>{title}</h1>
+        {/* it wont change the title on click */}
+        <button onClick={()=>setTitle("New Food App")}>Change title</button>
         <div className="nav-items">
           <ul>
             <li>Home</li>
@@ -16,6 +27,13 @@ const Header=()=>{
             <li>Cart</li>
           </ul>
         </div>
+        {
+          isLoggedIn?(
+            <button onClick={()=>setIsLoggedIn(false)}>Logout</button>
+          ):(
+            <button onClick={()=>setIsLoggedIn(true)}>Login</button>
+          )
+        }
       </div>
     )
   }
